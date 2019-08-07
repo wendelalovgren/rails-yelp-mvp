fcfd823eb129432cc86e27c0024915f4b4e531fc
Wendela Lövgren <wendela.lovgren@gmail.com>
2019-08-07 12:29:36 +0200

    done with models and validation

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, presence: true, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"] }
+  validates :phone_number, presence: true
+end
