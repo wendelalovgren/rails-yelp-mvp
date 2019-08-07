a27e5b199720e28a07290eb741ab03129bda3ff4
Wendela Lövgren <wendela.lovgren@gmail.com>
2019-08-07 16:36:26 +0200

    done w the functionality

--- Gemfile
+++ Gemfile
@@ -68,3 +68,5 @@ gem 'rails-controller-testing', group: [ :test ]
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
 gem 'simple_form'
+
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(
+    name: Faker::Cannabis.brand,
+    address: Faker::Address.street_address,
+    phone_number: Faker::Number.leading_zero_number(digits: 10),
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+    )
+end
